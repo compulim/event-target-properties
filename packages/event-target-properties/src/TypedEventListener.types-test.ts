4950ca7c58f57a96e64420ba28f83a9578d325e9
William Wong <compulim@users.noreply.github.com>
2025-12-31 06:43:36 +0000

    Fix ESLint

--- packages/event-target-properties/src/TypedEventListener.types-test.ts
+++ packages/event-target-properties/src/TypedEventListener.types-test.ts
@@ -4,3 +4,5 @@ const _1: TypedEventListener<CustomEvent<'load'>> = (_event: CustomEvent<'load'>
 
 // @ts-expect-error Type 'CustomEvent<"click">' is not assignable to type 'CustomEvent<"load">'.
 const _2: TypedEventListener<CustomEvent<'click'>> = (_event: CustomEvent<'load'>) => {};
+
+console.log(_1, _2);
--- packages/event-target-properties/src/assumption.test.ts
+++ packages/event-target-properties/src/assumption.test.ts
@@ -5,6 +5,7 @@ test('assume EventTarget do not natively support property-based event listener',
   const eventTarget = new EventTarget();
   const onload = mock.fn();
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (eventTarget as any).onload = onload;
   eventTarget.dispatchEvent(new CustomEvent('load'));
 
